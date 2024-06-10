884569e2783fb5dc215ffbb5f588ed0ba3c2f6d2
Liviu <liviuarsenescu@gmail.com>
2024-06-10 14:02:07 +0200

    feat(args): parse and send errors if arguments aren't propper

--- src/client_smtp.c
+++ src/client_smtp.c
@@ -5,6 +5,9 @@
 #include <netdb.h>
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sysexits.h>
+
+#define DEFAULT_PORT "25"
 
 static FILE *tcp_connect(const char *hostname, const char *port);
 
@@ -18,9 +21,74 @@ typedef enum {
     SMTP_SEND,
     SMTP_QUIT,
     CLOSE,
-} smtp_state;
+} smtp_state_t;
 
 int main(int argc, char **argv) {
+    /* 
+     * Command line arguments structure:
+     * 1: sender email address
+     * 2: mail subject
+     * 3: message body file name
+     * 4: DNS domain or IP address of the mail server
+     * 5: reciever mail address
+     * 6: *optional* port number
+     */
+
+    // Check for correct number of arguments
+    if(argc < 6 || argc > 7) {
+        fprintf(stderr, "[!] Usage: %s <sender email> <subject> <message file> <mail server> <reciever email> [<port>]\n", argv[0]);
+        exit(EX_USAGE);
+    }
+
+    // Copy each argument to a variable for readability
+    char *sender = argv[1];
+    char *subject = argv[2];
+    char *message_file = argv[3];
+    char *mail_server = argv[4];
+    char *reciever = argv[5];
+    char *port = (argc == 7) ? argv[6] : DEFAULT_PORT;
+
+    // Check if the arguments respect the RFC 3696 and RFC 1035 norms
+    // Check if the sender is a valid email address
+    if(strlen(sender) < 7 || strlen(sender) > 320) {
+        fprintf(stderr, "[!] Invalid sender email address: %s\n", sender);
+        exit(EX_DATAERR);
+    }
+    // Check if the subject is a certain length
+    if(strlen(subject) > 90) {
+        fprintf(stderr, "[!] Subject is too long\n");
+        exit(EX_DATAERR);
+    } else if(strlen(subject) == 0) {
+        fprintf(stderr, "[!] Subject is empty\n");
+        exit(EX_DATAERR);
+    }
+    // Check file name length
+    if(strlen(message_file) > 256) {
+        fprintf(stderr, "[!] Message file name is too long\n");
+        exit(EX_DATAERR);
+    } else if(strlen(message_file) == 0) {
+        fprintf(stderr, "[!] Message file name is empty\n");
+        exit(EX_DATAERR);
+    }
+    // Check if the mail server is a valid domain name or IP address
+    if(strlen(mail_server) < 7 || strlen(mail_server) > 255) {
+        fprintf(stderr, "[!] Invalid mail server: %s\n", mail_server);
+        exit(EX_DATAERR);
+    }
+    // Check if the reciever is a valid email address
+    if(strlen(reciever) < 7 || strlen(reciever) > 320) {
+        fprintf(stderr, "[!] Invalid reciever email address: %s\n", reciever);
+        exit(EX_DATAERR);
+    }
+    // Check if the port number is valid
+    if(strlen(port) > 4) {
+        fprintf(stderr, "[!] Invalid port number: %s\n", port);
+        exit(EX_DATAERR);
+    } else if(strlen(port) == 0) {
+        strncpy(port, DEFAULT_PORT, 4);
+    }
+
+    return EXIT_SUCCESS;
 }
 
 #define LOW_LEVEL_DEBUG
